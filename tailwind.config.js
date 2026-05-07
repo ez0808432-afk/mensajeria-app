0d990b063157039831f79e812deccda91e49e3ca
Elvis Zambrano <ez0808432@gmail.com>
2026-05-07 12:16:39 -0500

    mejora responsive chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,jsx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
